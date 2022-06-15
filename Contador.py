3a9f25f83176f36ae4e41cb59e9d94d53f6cc26f
FernandoPerera <fernando02perera@gmail.com>
2022-06-15 14:07:27 +0100

    Feature: Test rojo

---
+++ Contador.py
@@ -0,0 +1,6 @@
+class Contador:
+    def __init__(self, valorLimite, valorInicial=0, valorIncremento=1):
+        self.contador = valorInicial
+        self.valorInicial = valorInicial
+        self.valorIncremento = valorIncremento
+        self.valorLimite = valorLimite
---
+++ TestContador.py
@@ -0,0 +1,22 @@
+import unittest
+from Contador import Contador
+
+class TestContador(unittest.TestCase):
+    
+    '''
+        Clase para hacer los test  de la clase Contador (Contador.py)
+    '''
+    
+    def test_constructor(self):
+        '''
+            Funcion que contiene el test para comprobar que el constructor recoge correctamente los parámetros. 
+        '''
+        contador = Contador(10, 2, 2)
+        self.assertEqual(contador.valorLimite, 10)
+        self.assertEqual(contador.valorLimite, 2)
+        self.assertEqual(contador.valorIncremento, 2)
+    
+    
+
+if __name__ == '__main__':
+    unittest.main()
